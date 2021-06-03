f2d7d470fae13dfc42ca73c41e70332d55e4e6f2
ManjitPython <manjisca@gmail.com>
2021-06-03 12:09:10 -0700

    First commit

---
+++ SetPython.py
@@ -0,0 +1,43 @@
+# l = (1,22,22,33,44)
+# print(l)
+#
+# lst = ["chinmay","chinmay"]
+# print(lst)
+#
+#
+# person = {
+#
+#     'name':'chinmay',
+#     'name':"poorva"
+#
+#
+# }
+#
+# print(person)
+#
+#
+setA = {1,2,3,333,444,444,4,3}
+# print(setA)
+
+
+#
+
+setA.add(9)
+# print(setA)
+#
+setA.remove(444)
+print(setA)
+#
+# setA.pop()
+# print(setA)
+#
+# tupleA = (1,2)
+# tupleA = (5,7)
+#
+# # string and tuple are immutable
+# setA
+# setA.clear()
+# print(setA)
+#
+# b = set([44,55,44,66,55])
+# print(list(b))
---
+++ Tuple.py
@@ -0,0 +1,45 @@
+# tuple
+
+
+# tuple = (1,3,4,5)
+# len(tuple)
+#
+#
+# for item in tuple:
+#     print(item)
+#
+# for item in range(len(tuple)):
+#     print(item)
+#     print(tuple[item])
+
+
+
+# lst = ["c","s","d"]
+# lst[0] = "t"
+# print(lst)
+
+# lst = ("c","s","d")
+# lst[0] = "t"
+# print(lst)
+
+# converting tuple into the list and list into tuple
+
+
+# b = (12,13)
+# print(type(b))
+#
+# c = 10,
+# print(type(c))
+#
+# d = 12,13
+# print(type(d))
+
+#
+# def add(x,y):
+#     return x ,y
+#
+# r = add(12,13)
+# print(r[0])
+# print(r[1])
+# print(type(r))
+# # methods on tuple
