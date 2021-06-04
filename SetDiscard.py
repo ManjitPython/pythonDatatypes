dd3f3d2190fc8d9a7a7cc508e00dbe98ff5ac53e
ManjitPython <manjisca@gmail.com>
2021-06-04 09:36:19 -0700

    commit in some lines deleted.

--- SetDiscard.py
+++ SetDiscard.py
@@ -1,11 +1,12 @@
 
 setA = {1,22,33}
 setB = {4,5,6}
-print(setA.isdisjoint(setB))
-print(setB.isdisjoint(setA))
-print(5,6,7)
+# print(setA.isdisjoint(setB))
+# print(setB.isdisjoint(setA))
+# print(5,6,7)
 setA.discard(22)
 setB.discard(5)
 print(setA)
 print(setB)
 print(setA,setB)
+print('testing')
